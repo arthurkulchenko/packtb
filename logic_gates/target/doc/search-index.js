c42fb2b6a88e66a2849361e481fb2f20bb37041c
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-07-19 01:06:33 +0300

    remove doc

--- logic_gates/target/doc/search-index.js
+++
@@ -1,5 +0,0 @@
-var searchIndex = JSON.parse('{\
-"logic_gates":{"doc":"","t":"FF","n":["and","xor"],"q":[[0,"logic_gates"]],"d":["",""],"i":[0,0],"f":[[[1,1],1],[[1,1],1]],"c":[],"p":[[15,"u8"]]}\
-}');
-if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
-if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
