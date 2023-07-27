bbe1264928d468c15c7903a63aed3163ad5197be
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-07-27 23:21:10 +0300

    development:

--- hyperurl/src/main.rs
+++ hyperurl/src/main.rs
@@ -1,20 +1,52 @@
-use log::{info, error};
+use log::{info};
 use std::env;
-use hyper::Server;
+// use hyper::server;
 use hyper::service::service_fn;
-use hyper::rt::{self, Future};
+use tokio::net::TcpListener;
+// use hyper::rt::{self};
+use hyper::server::conn::http1;
+use hyper::{Request, Response};
+use http_body_util::Full;
+use hyper::body::Bytes;
+use std::convert::Infallible;
+// use std::net::SocketAddr;
 
 mod shortener;
 mod service;
 
-use crate::service::url_service;
+// use crate::service::url_service;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+// #[tokio::main]
+// async fn main() {
     env::set_var("RUST_LOG", "hyperurl=info");
     pretty_env_logger::init();
 
+    // let addr = SocketAddr::from(([127, 0, 0, 1], 3002));
     let addr = "127.0.0.1:3002".parse().unwrap();
-    let server = Server::bind(&addr).serv(|| service_fn(url_service)).map_err(|e| error!("server error: {}", e));
+    // let server = Server::bind(&addr).serv(|| service_fn(url_service)).map_err(|e| error!("server error: {}", e));
+    // let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    // We create a TcpListener and bind it to 127.0.0.1:3000
+    let listener = TcpListener::bind(addr).await?;
     info!("URL shortener listening on http://{}", addr);
-    rt::run(server);
+    // rt::run(server);
+    // server.await;
+    loop {
+        let (stream, _) = listener.accept().await?;
+        // Spawn a tokio task to serve multiple connections concurrently
+        tokio::task::spawn(async move {
+            // Finally, we bind the incoming connection to our `hello` service
+            if let Err(err) = http1::Builder::new()
+                // `service_fn` converts our function in a `Service`
+                // .serve_connection(stream, service_fn(url_service))
+                .serve_connection(stream, service_fn(hello))
+                .await
+            { println!("Error serving connection: {:?}", err); }
+        });
+    }
+}
+
+async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
 }
--- hyperurl/src/shortener.rs
+++ hyperurl/src/shortener.rs
@@ -1,1 +1,10 @@
+use crypto::digest::Digest;
+use crypto::sha2::Sha256;
 
+pub(crate) fn shorten_url(url: &str) -> String {
+  let mut sha = Sha256::new();
+  sha.input_str(url);
+  let mut new_url = sha.result_str();
+  new_url.truncate(5);
+  format!("https://u.rl/{}", new_url)
+}
