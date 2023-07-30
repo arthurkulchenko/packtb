e773d766b956375c2a8f48c8a63baef697f3897c
Artur <12735101+arthurkulchenko@users.noreply.github.com>
2023-07-30 19:23:54 +0300

    Pospone hyperurl (#7)
    * hyperurl: WIP(not working)
    ---------
    Co-authored-by: せんぼんざくらかげよし <artur.kulchenko@pm.me>

--- hyperurl/src/main.rs
+++ hyperurl/src/main.rs
@@ -1,52 +1,83 @@
 use log::{info};
 use std::env;
-// use hyper::server;
-use hyper::service::service_fn;
-use tokio::net::TcpListener;
-// use hyper::rt::{self};
-use hyper::server::conn::http1;
-use hyper::{Request, Response};
-use http_body_util::Full;
-use hyper::body::Bytes;
+
 use std::convert::Infallible;
-// use std::net::SocketAddr;
+use std::net::SocketAddr;
 
-mod shortener;
-mod service;
+use http_body_util::Full;
+use tokio::net::TcpListener;
 
-// use crate::service::url_service;
+use hyper::body::Bytes;
+// use hyper::service::service_fn;
+// use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+// use hyper::body;
+
+use std::sync::RwLock;
+use core::future::Future;
+use std::collections::HashMap;
+use std::sync::{Arc};
+
+use lazy_static::lazy_static;
+// use crate::shortener::shorten_url;
+
+type UrlDb = Arc<RwLock<HashMap<String, String>>>;
+lazy_static! { static ref SHORT_URLS: UrlDb = Arc::new(RwLock::new(HashMap::new())); }
+
+// async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+//     Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+// }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-// #[tokio::main]
-// async fn main() {
+async fn main() {
     env::set_var("RUST_LOG", "hyperurl=info");
     pretty_env_logger::init();
 
-    // let addr = SocketAddr::from(([127, 0, 0, 1], 3002));
-    let addr = "127.0.0.1:3002".parse().unwrap();
-    // let server = Server::bind(&addr).serv(|| service_fn(url_service)).map_err(|e| error!("server error: {}", e));
-    // let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    // We create a TcpListener and bind it to 127.0.0.1:3000
-    let listener = TcpListener::bind(addr).await?;
-    info!("URL shortener listening on http://{}", addr);
-    // rt::run(server);
-    // server.await;
-    loop {
-        let (stream, _) = listener.accept().await?;
-        // Spawn a tokio task to serve multiple connections concurrently
-        tokio::task::spawn(async move {
-            // Finally, we bind the incoming connection to our `hello` service
-            if let Err(err) = http1::Builder::new()
-                // `service_fn` converts our function in a `Service`
-                // .serve_connection(stream, service_fn(url_service))
-                .serve_connection(stream, service_fn(hello))
-                .await
-            { println!("Error serving connection: {:?}", err); }
-        });
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let make_svc = make_service_fn(|_conn| async {
+        // service_fn converts our function into a `Service`
+        Ok::<_, Infallible>(service_fn(url_service))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    // Run this server for... forever!
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
     }
+
+    // let listener = TcpListener::bind(addr).await?;
+    // loop {
+    //     let (stream, _) = listener.accept().await?;
+    //     let io = TokioIo::new(stream);
+    //     tokio::task::spawn(async move {
+    //         if let Err(err) = http1::Builder::new().serve_connection(io, service_fn(url_service)).await {
+    //             println!("Error serving connection: {:?}", err);
+    //         }
+    //     });
+    // }
 }
 
-async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+async fn url_service(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let res = req.into_body();
+    info!("Request: {:?}", res);
+    // let reply = req.map(move |chunk| {
+    //   info!("Request: {:?}", chunk);
+
+    //   // let url_to_shorten = std::str::from_utf8(&chunk).unwrap();
+    //   // Now you have the body as a String and you can work with it.
+    //   // let shortened_url = shorten_url(url_to_shorten);
+    //   // SHORT_URLS.write().unwrap().insert(shortened_url, url_to_shorten.to_string());
+    //   // let a = &*SHORT_URLS.read().unwrap();
+    //   // Response::new(Full::new(Bytes::from(format!("{:#?}", a))))
+    // });
+
+
+    // let entire_body = body::to_bytes(req.into_body()).await?;
+
+    // println!("Received body: {:?}", entire_body);
+   Ok(Response::new("Hello, World!".into()))
 }
