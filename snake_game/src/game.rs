2a194a48c983a94f01f1e48d745092416ba44304
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-08-07 02:14:48 +0300

    development: snake_game: add food

--- snake_game/src/game.rs
+++ snake_game/src/game.rs
@@ -5,18 +5,26 @@ use rand::{thread_rng, Rng};
 use crate::draw::{draw_block, draw_rectangle};
 
 const BORDER_COLOR: Color = [0.0, 0.0, 0.0, 1.0,];
+const FOO_COLOR: Color = [0.8, 0.0, 0.0, 1.0,];
 
 pub struct Game {
+    food_exists: bool,
+    food_x: i32,
+    food_y: i32,
+
     width: i32,
     height: i32
 }
 
 impl Game {
     pub fn new(width: i32, height: i32) -> Game {
-        Game { width, height }
+        Game { food_exists: false, food_x: 0, food_y: 0, width, height }
     }
 
     pub fn draw(&self, con: &Context, g: &mut G2d) {
+      if self.food_exists {
+        draw_block(FOO_COLOR, self.food_x, self.food_y, con, g)
+      }
       draw_rectangle(BORDER_COLOR, 0, 0, self.width, 1, con, g);
       draw_rectangle(BORDER_COLOR, 0, self.height - 1, self.width, 1, con, g);
       draw_rectangle(BORDER_COLOR, 0, 0, 1, self.height, con, g);
@@ -24,6 +32,17 @@ impl Game {
     }
 
     pub fn update(&mut self, delta_time: f64) {
+        if !self.food_exists {
+            self.add_food();
+        }
+    }
 
+    fn add_food(&mut self) {
+        let mut rng = thread_rng();
+        let mut new_x = rng.gen_range(1..self.width - 1);
+        let mut new_y = rng.gen_range(1..self.height - 1);
+        self.food_x = new_x;
+        self.food_y = new_y;
+        self.food_exists = true;
     }
 }
