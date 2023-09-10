70e70fa6cb554d0a7dc823ccc248519237fe52bb
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-09-10 23:47:08 +0300

    development: hands on: Doubly Linked List: add push prepend methods

--- hands_on/doubly_linked_list/src/main.rs
+++ hands_on/doubly_linked_list/src/main.rs
@@ -1,11 +1,26 @@
 use std::rc::{Rc, Weak};
 use core::cell::RefCell;
 
-type NodeRef<T> = Rc<RefCell<Node<T>>>;
+#[derive(Debug)]
+struct Node<T> {
+    data: T,
+    next: Option<NextNode<T>>,
+    prev: Option<Weak<RefCell<Node<T>>>>,
+}
 
+impl<T> Node<T> {
+    fn new_list_node(data: T, prev: Option<PrevNode<T>>, next: Option<NextNode<T>>) -> NextNode<T> {
+        Rc::new(RefCell::new(Node { data, next, prev }))
+    }
+}
+
+type NextNode<T> = Rc<RefCell<Node<T>>>;
+type PrevNode<T> = Weak<RefCell<Node<T>>>;
+
+#[derive(Debug)]
 pub struct DoublyLinkedList<T> {
-    head: Option<NodeRef<T>>,
-    tail: Option<Weak<RefCell<Node<T>>>>,
+    head: Option<NextNode<T>>,
+    tail: Option<PrevNode<T>>,
 }
 
 impl<T> DoublyLinkedList<T> {
@@ -13,14 +28,36 @@ impl<T> DoublyLinkedList<T> {
         DoublyLinkedList { head: None, tail: None }
     }
 
-    fn push_front(&mut self, data: T) {
-        match self.head.take() {
+    fn push(&mut self, data: T) {
+        let prev_node = self.tail.take();
+        match prev_node {
             Some(node) => {
-                // let new_node = Rc::new(RefCell::new(Node { data, next: Some(node.clone), prev: None, })); 
-                let new_node = Rc::new(RefCell::new(Node { data, next: Some(node), prev: None, })); 
+                let tail_node = Weak::upgrade(&node).unwrap();
+                let mut mutable_tail_node = tail_node.borrow_mut();
+
+                let new_node = Node::new_list_node(data, Some(node.clone()), None); // NextNode
+                self.tail = Some(Rc::downgrade(&new_node));
+                mutable_tail_node.next = Some(new_node);
             },
             None => {
-                let new_node = Rc::new(RefCell::new(Node { data, next: None, prev: None, }));
+                let new_node = Node::new_list_node(data, None, None); // NextNode
+                self.tail = Some(Rc::downgrade(&new_node));
+                self.head = Some(new_node);
+            }
+        }
+    }
+
+    fn prepend(&mut self, data: T) {
+        match self.head.take() {
+            Some(head_node) => {
+                let mut mutable_head_node = head_node.borrow_mut();
+
+                let new_node = Node::new_list_node(data, None, Some(head_node.clone())); // NextNode
+                self.head = Some(new_node.clone());
+                mutable_head_node.prev = Some(Rc::downgrade(&new_node));
+            },
+            None => {
+                let new_node = Node::new_list_node(data, None, None); // NextNode
                 self.tail = Some(Rc::downgrade(&new_node));
                 self.head = Some(new_node);
             }
@@ -28,13 +65,10 @@ impl<T> DoublyLinkedList<T> {
     }
 }
 
-#[derive(Debug)]
-struct Node<T> {
-    data: T,
-    next: Option<NodeRef<T>>,
-    prev: Option<Weak<RefCell<Node<T>>>>,
-}
-
 fn main() {
-    println!("Hello, world!");
+    let mut linked_list = DoublyLinkedList::new();
+    linked_list.push(5);
+    linked_list.push(2);
+    linked_list.prepend(1);
+    println!("{:?}", linked_list);
 }
