feae6dec5f49c82ed7dc69e476c7d533cb4ccbd9
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-09-23 23:34:00 +0300

    development: hands on: add finding shortest path

--- hands_on/graphs/src/route_structure.rs
+++ hands_on/graphs/src/route_structure.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::hash::Hash;
 use std::rc::Rc;
 use std::fmt;
@@ -24,16 +24,18 @@ impl Weighted for i32 {
     }
 }
 
+type RRoute<ID> = Rc<Route<ID>>;
+
 #[derive(Debug)]
 pub struct Route<ID> {
     position: ID,
     path: Option<Rc<Route<ID>>>,
-    lenght: i32
+    length: i32
 }
 
 impl <ID: Eq> Route<ID> {
     pub fn start_rc(position: ID) -> Rc<Self> {
-        Rc::new(Route { position, path: None, lenght: 0 })
+        Rc::new(Route { position, path: None, length: 0 })
     }
 
     pub fn contains(&self, id: &ID) -> bool {
@@ -50,7 +52,7 @@ impl <ID: Eq> Route<ID> {
 impl<ID: fmt::Debug> fmt::Display for Route<ID> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         if let Some(ref path) = self.path {
-            return write!(f, "{}--{}", path, self.lenght);
+            return write!(f, "{}--{}", path, self.length);
         }
         write!(f, "{:?}", self.position)
     }
@@ -88,6 +90,53 @@ impl<T, E, ID: Clone + Hash + Eq> Graph<T, E, ID> {
     }
 }
 
+impl <T, E: Weighted, ID: Clone + Hash + Eq> Graph<T,E,ID> {
+    pub fn shortest_path(&self, from: ID, to: ID) -> Option<RRoute<ID>> {
+        let mut visited: HashSet<ID> = HashSet::new();
+        let mut routes = Vec::new();
+        routes.push(Route::start_rc(from));
+
+        loop {
+            let current_route = routes.pop()?;
+            if to == current_route.position { return Some(current_route); }
+            if visited.contains(&current_route.position) { continue; }
+
+            visited.insert(current_route.position.clone());
+            let exist = self.data.get(&current_route.position)?;
+
+            for existed_id in &exist.1 {
+                let edge = self.edges.get(existed_id)?;
+                // WHAT: What is npos
+                let npos = if edge.1 == current_route.position {
+                    edge.2.clone()
+                } else {
+                    edge.1.clone()
+                };
+                let nlen = current_route.length + edge.0.weight();
+                let nroute = Rc::new(Route { position: npos, length: nlen, path: Some(current_route.clone()) });
+
+                if routes.len() == 0 {
+                    routes.push(nroute.clone());
+                    continue;
+                }
+
+                let mut index_after = routes.len() - 1;
+                loop {
+                    if routes[index_after].length > nlen {
+                        routes.insert(index_after + 1, nroute);
+                        break;
+                    }
+                    if index_after == 0 {
+                        routes.insert(0, nroute);
+                        break;
+                    }
+                    index_after -= 1;
+                }
+            }
+        }
+    }
+}
+
 fn main() -> Result<(), GraphErr> {
     let mut graph = Graph::new();
     for x in vec!['A', 'B', 'C', 'D', 'E', 'F', 'G', 'F', 'H'] {
@@ -102,6 +151,8 @@ fn main() -> Result<(), GraphErr> {
     let route = Route::start_rc('A');
 
     println!("{}", route);
+    println!("shortest_path A-D is {}", graph.shortest_path('A', 'D').unwrap());
+    println!("shortest_path H-B is {}", graph.shortest_path('H', 'D').unwrap());
 
     Ok(())
 }
