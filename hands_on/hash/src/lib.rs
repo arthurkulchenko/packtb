7efb7f47759a35ae1f461ea93874a7e73f54ae42
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-09-25 20:57:39 +0300

    development: hand on: intermediate commit

--- hands_on/hash/src/lib.rs
+++ hands_on/hash/src/lib.rs
@@ -18,17 +18,32 @@ impl <K: Hash + Eq, V> BucketList<K,V> {
     }
 
     fn push(&mut self, key: K, value: V) -> usize {
-        let h = (hash(self.seed, &key) as usize) % self.buckets.len();
-        self.buckets[h].push((key, value));
+        let hash_index = (hash(self.seed, &key) as usize) % self.buckets.len();
+        self.buckets[hash_index].push((key, value));
         self.length += 1;
-        self.buckets[h].len()
+        self.buckets[hash_index].len()
     }
 
-    fn get<KB>(&self, k: &KB) -> Option<&V> where K: Borrow<KB>, KB: Hash + Eq + ?Sized  {
-        let h = (hash(self.seed, &k) as usize) % self.buckets.len();
-        for (inner_key, inner_value) in &self.buckets[h] {
-            if k == inner_key.borrow() { return Some(inner_value) }
+    fn get_mut<KB>(&mut self, k: &KB) -> Option<&V> where K: Borrow<KB>, KB: Hash + Eq + ?Sized  {
+        let hash_index = (hash(self.seed, &k) as usize) % self.buckets.len();
+        for (inner_key, inner_value) in &mut self.buckets[hash_index] {
+            if k == (inner_key as &K).borrow() { return Some(inner_value) }
         }
         None
+    }
+
+    fn bucket_pop(&mut self, index: usize) -> Option<Vec<(K,V)>> {
+        if index >= self.buckets.len() { return None }
+
+        let mut result = Vec::new();
+        std::mem::swap(&mut result, &mut self.buckets[index]);
+        self.length -= result.len();
+        Some(result)
+    }
+
+    fn set_buckets(&mut self, index: usize) {
+        for _ in self.buckets.len()..index {
+            self.buckets.push(Vec::new());
+        }
     }
 }
