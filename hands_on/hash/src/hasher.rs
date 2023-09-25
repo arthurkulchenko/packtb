36ac76ff2b51ab9e00d8210a60654cfb94809bf1
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-09-25 04:39:13 +0300

    development: hand on: adding pop and set buckets

--- hands_on/hash/src/hasher.rs
+++ hands_on/hash/src/hasher.rs
@@ -2,24 +2,24 @@ use std::hash::{Hash, Hasher};
 
 pub struct Mhash {
     prev: u8,
-    n: u128,
+    num: u128,
 }
 
 impl Hasher for Mhash {
-    fn write(&mut self, dt: &[u8]) {
-        for d in dt {
-            self.n = (((self.n + 11) * (*d as u128 + 13) + (d ^ self.prev) as u128)) % (std::u64::MAX as u128);
-            self.prev = *d;
+    fn write(&mut self, data: &[u8]) {
+        for datum in data {
+            self.num = (((self.num + 11) * (*datum as u128 + 13) + (datum ^ self.prev) as u128)) % (std::u64::MAX as u128);
+            self.prev = *datum;
         }
     }
 
     fn finish(&self) -> u64 {
-        self.n as u64
+        self.num as u64
     }
 }
 
-pub fn hash<T:Hash>(seed: u64, t: T) -> u64 {
-    let mut h = Mhash { n: 0, prev: 0 };
+pub fn hash<T: Hash>(seed: u64, t: T) -> u64 {
+    let mut h = Mhash { num: 0, prev: 0 };
     h.write_u64(seed);
     t.hash(&mut h);
     h.finish()
@@ -32,7 +32,7 @@ mod test {
 
     #[test]
     pub fn test_hasher() {
-        let n = hash(55, "cat");
-        assert_eq!(n, hash(55, "cat"));
+        let num = hash(55, "cat");
+        assert_eq!(num, hash(55, "cat"));
     }
 }
--- hands_on/hash/src/lib.rs
+++ hands_on/hash/src/lib.rs
@@ -1,1 +1,34 @@
 mod hasher;
+
+use rand;
+use std::hash::Hash;
+use hasher::hash;
+use std::borrow::Borrow;
+
+#[derive(Debug)]
+pub struct BucketList<K,V> {
+    length: usize,
+    buckets: Vec<Vec<(K,V)>>,
+    seed: u64
+}
+
+impl <K: Hash + Eq, V> BucketList<K,V> {
+    fn new() -> Self {
+        Self { length: 0, buckets: vec![Vec::new()], seed: rand::random() }
+    }
+
+    fn push(&mut self, key: K, value: V) -> usize {
+        let h = (hash(self.seed, &key) as usize) % self.buckets.len();
+        self.buckets[h].push((key, value));
+        self.length += 1;
+        self.buckets[h].len()
+    }
+
+    fn get<KB>(&self, k: &KB) -> Option<&V> where K: Borrow<KB>, KB: Hash + Eq + ?Sized  {
+        let h = (hash(self.seed, &k) as usize) % self.buckets.len();
+        for (inner_key, inner_value) in &self.buckets[h] {
+            if k == inner_key.borrow() { return Some(inner_value) }
+        }
+        None
+    }
+}
