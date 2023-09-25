3d57ba2b6ada78a8460bb126889ed77a820b696c
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-09-25 23:13:02 +0300

    evelopment: hand on: add Hmap

--- hands_on/hash/src/lib.rs
+++ hands_on/hash/src/lib.rs
@@ -32,6 +32,14 @@ impl <K: Hash + Eq, V> BucketList<K,V> {
         None
     }
 
+    fn get<KB>(&self, k: &KB) -> Option<&V> where K: Borrow<KB>, KB: Hash + Eq + ?Sized  {
+        let hash_index = (hash(self.seed, &k) as usize) % self.buckets.len();
+        for (inner_key, inner_value) in &self.buckets[hash_index] {
+            if k == (inner_key as &K).borrow() { return Some(inner_value) }
+        }
+        None
+    }
+
     fn bucket_pop(&mut self, index: usize) -> Option<Vec<(K,V)>> {
         if index >= self.buckets.len() { return None }
 
@@ -47,3 +55,77 @@ impl <K: Hash + Eq, V> BucketList<K,V> {
         }
     }
 }
+
+#[derive(Debug)]
+pub struct Hmap<K, V> {
+    n_moved: usize,
+    main: BucketList<K,V>,
+    aux: BucketList<K,V> // grow
+}
+
+const BSIZE: usize = 8;
+const BAUXSIZE: usize = 8;
+
+impl <K: Hash + Eq, V> Hmap<K,V> {
+    pub fn new() -> Self {
+        Self { n_moved: 0, main: BucketList::new(), aux: BucketList::new() }
+    }
+
+    pub fn insert(&mut self, k: K, v: V) {
+        if let Some(inner_value) = &mut self.main.get_mut(&k) {
+            *inner_value = &v;
+            return;
+        }
+        if let Some(inner_value) = &mut self.aux.get_mut(&k) {
+            *inner_value = &v;
+            return;
+        }
+        if self.n_moved > 0 {
+            self.aux.push(k, v);
+            self.move_buacket();
+            return;
+        }
+        if self.main.push(k, v) > BSIZE / 2 {
+            self.move_buacket();
+        }
+    }
+
+    pub fn get_mut<KR>(&mut self, kr: &mut KR) -> Option<& V> where K: Borrow<KR>, KR: Hash + Eq + ?Sized {
+        if let Some(inner_value) = self.main.get_mut(kr) { return Some(inner_value); }
+        self.aux.get_mut(kr)
+    }
+
+    pub fn get<KR>(&self, kr: &KR) -> Option<&V> where K: Borrow<KR>, KR: Hash + Eq + ?Sized {
+        self.main.get(kr).or_else(|| self.aux.get(kr))
+    }
+
+    pub fn len(&self) -> usize {
+        self.main.buckets.len() + self.aux.buckets.len()
+    }
+
+    pub fn move_buacket(&mut self) {
+        if self.n_moved == 0 {
+            self.aux.set_buckets(self.main.buckets.len() * BAUXSIZE);
+        }
+        if let Some(inner_value) = self.main.bucket_pop(self.n_moved) {
+            for (k, v) in inner_value {
+                self.aux.push(k, v);
+            }
+            self.n_moved += 1;
+            return;
+        }
+        std::mem::swap(&mut self.main, &mut self.aux);
+        self.n_moved = 0;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
