b74bf6bdfd21cac559ca37ccaf5e8a3470360c8c
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-09-29 21:23:15 +0300

    development: hand on: WIP

--- hands_on/quick-sorting/src/lib.rs
+++ hands_on/quick-sorting/src/lib.rs
@@ -34,34 +34,45 @@ pub fn quick_sort<T: PartialOrd>(v: &mut [T]) {
 
 // pub fn threded_quick_sort<T: PartialOrd + Send>(v: & mut [T]) {
 // NOTICE: 'static lifetime seems to do the thing without raw pointer
-pub fn threded_quick_sort<T: 'static + PartialOrd + Send>(v: &mut [T]) {
+// pub fn threded_quick_sort<T: 'static + PartialOrd + Send>(v: &mut [T]) {
+//     if v.len() > 1 { return }
+
+//     let p = pivot(v);
+//     let (a, b) = v.split_at_mut(p);
+//     let handle = std::thread::spawn(move || {
+//         let mut a = a;
+//         threded_quick_sort(&mut a);
+//         a
+//     });
+//     threded_quick_sort(b);
+//     let a_sorted = handle.join().unwrap();
+//     for i in 0..a_sorted.len() {
+//         v[i] = a_sorted[i];
+//     }
+// }
+
+pub fn quick_sort_rayon<T: PartialOrd + Send>(v: &mut [T]) {
     if v.len() > 1 {
-        let p = pivot(v);
-        // quick_sort(&mut v[..p]);
-        // quick_sort(&mut v[p+1..]);
-        let (a, b) = v.split_at_mut(p);
-
-        let handle = std::thread::spawn(move || threded_quick_sort(a));
-        // let raw_p: *mut [T] = a as *mut [T];
-        // let raw_a = RawSend(raw_p);
-        // let handle = std::thread::spawn(move || threded_quick_sort(&mut *raw_a.0));
-
-        threded_quick_sort(b);
-        // quick_sort(a);
-        // quick_sort(&mut b[1..]);
-        handle.join().ok();
+        return;
     }
+    let p = pivot(v);
+    let (a, b) = v.split_at_mut(p);
+    rayon::join(|| quick_sort_rayon(a), || quick_sort_rayon(&mut b[1..]));
 }
+
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn quick_sort_test() {
+    pub fn quick_sort_test() {
         let mut v = vec![13, 4, 6, 1, 8, 11, 3];
         // quick_sort(&mut v);
-        threded_quick_sort(&mut v);
+        // threded_quick_sort(&mut v);
+        quick_sort_rayon(&mut v);
+
         assert_eq!(v, [1, 3, 4, 6, 8, 11, 13]);
     }
 }
