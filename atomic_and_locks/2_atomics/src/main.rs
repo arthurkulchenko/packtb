98eba7689c5a2ba8357721b7e3b3e5ff9e993ef1
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-10-08 01:45:12 +0300

    development: atomic_and_locks: atomics: add exmapl of a Stop Flag

--- atomic_and_locks/2_atomics/src/main.rs
+++ atomic_and_locks/2_atomics/src/main.rs
@@ -1,3 +1,29 @@
+use std::thread;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering::Relaxed;
+
 fn main() {
-    println!("Hello, world!");
+    static STOP: AtomicBool = AtomicBool::new(false);
+
+    let background_thread = thread::spawn(|| {
+        // Will let finish next iteraiton of loop
+        while !STOP.load(Relaxed) {
+            some_work();
+        }
+    });
+
+    for line in std::io::stdin().lines() {
+        match line.unwrap().as_str() {
+            "help" => println!("commands: help, stop"),
+            "stop" => break,
+            cmd => println!("unknown command: {cmd:?}"),
+        }
+    }
+    STOP.store(true, Relaxed);
+    background_thread.join().unwrap();
+}
+
+fn some_work() {
+    let _ = std::thread::sleep(std::time::Duration::from_secs(6));
+    println!("working...");
 }
