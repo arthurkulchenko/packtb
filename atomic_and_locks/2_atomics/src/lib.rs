a868eef6932d07db0182bcac3ee5ec54c5d3ee7a
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-10-09 02:17:31 +0300

    development: atomic_and_locks: atomics: progress reporting + syncronization with park_timeout() + unpark() instead of sleep

--- atomic_and_locks/2_atomics/src/lib.rs
+++ atomic_and_locks/2_atomics/src/lib.rs
@@ -1,30 +1,28 @@
-// use std::io::Write;
 use std::sync::atomic::Ordering::Relaxed;
 use std::sync::atomic::AtomicUsize;
-// use std::thread::yield_now;
 
 fn main() {
     static NUM_DONE: AtomicUsize = AtomicUsize::new(0);
+    let main_thread = std::thread::current();
 
     // std::thread::scope(|s| {
         // s.spawn(|| {
-        let t = std::thread::spawn(|| {
-            for i in 0..100 {
+        let t = std::thread::spawn(move || {
+            for i in 0..10 {
                 process(i);
                 NUM_DONE.store(i + 1, Relaxed);
+                main_thread.unpark();
             }
         });
     // });
 
-    // println!("before main loop");
     loop {
-        // println!("main loop");
         let n = NUM_DONE.load(Relaxed);
-        if n == 100 { break; };
+        if n == 10 { break; };
 
-        println!("working.. {n}/100 done");
-        // std::io::stdout().flush().unwrap();
-        std::thread::sleep(std::time::Duration::from_secs(1));
+        println!("working.. {n}/10 done");
+        // std::thread::sleep(std::time::Duration::from_secs(1));
+        std::thread::park_timeout(std::time::Duration::from_secs(1));
     }
     t.join().unwrap();
     println!("done!");
@@ -32,5 +30,4 @@ fn main() {
 
 fn process(_index: usize) {
     std::thread::sleep(std::time::Duration::from_millis(400));
-    // yield_now();
 }
