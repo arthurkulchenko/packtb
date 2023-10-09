757d29db52801267712ee578318179e41315bdee
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-10-10 01:19:39 +0300

    development: atomic_and_locks: atomics: progress_report fix

--- atomic_and_locks/2_atomics/src/lib.rs
+++ atomic_and_locks/2_atomics/src/lib.rs
@@ -1,33 +1,39 @@
 use std::sync::atomic::Ordering::Relaxed;
 use std::sync::atomic::AtomicUsize;
+use std::thread;
 
 fn main() {
-    static NUM_DONE: AtomicUsize = AtomicUsize::new(0);
-    let main_thread = std::thread::current();
+    // static NUM_DONE: AtomicUsize = AtomicUsize::new(0);
+    let num_done: AtomicUsize = AtomicUsize::new(0);
+    // let main_thread = std::thread::current();
 
-    // std::thread::scope(|s| {
-        // s.spawn(|| {
-        let t = std::thread::spawn(move || {
+    thread::scope(|s| {
+        s.spawn(|| {
+        // let t = thread::spawn(move || {
             for i in 0..10 {
                 process(i);
-                NUM_DONE.store(i + 1, Relaxed);
-                main_thread.unpark();
+                // num_done.fetch_add(1, Relaxed);
+                num_done.store(i + 1, Relaxed);
+                // NUM_DONE.store(i + 1, Relaxed);
+                // main_thread.unpark();
             }
         });
     // });
 
     loop {
-        let n = NUM_DONE.load(Relaxed);
+        // let n = NUM_DONE.load(Relaxed);
+        let n = num_done.load(Relaxed);
         if n == 10 { break; };
 
         println!("working.. {n}/10 done");
-        // std::thread::sleep(std::time::Duration::from_secs(1));
-        std::thread::park_timeout(std::time::Duration::from_secs(1));
+        thread::sleep(std::time::Duration::from_secs(1));
+        // thread::park_timeout(std::time::Duration::from_secs(1));
     }
-    t.join().unwrap();
+    // t.join().unwrap();
+    }); // NOTICE: Loop now insize thread scope
     println!("done!");
 }
 
 fn process(_index: usize) {
-    std::thread::sleep(std::time::Duration::from_millis(400));
+    thread::sleep(std::time::Duration::from_millis(400));
 }
