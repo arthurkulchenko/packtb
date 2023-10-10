8c5c43b0363a7052ef3953ba047ddfabbbadf45d
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-10-10 20:53:31 +0300

    development: oreilly_system_design: cli: restructure quick replace

--- oreilly_system_design/2_quickreplace/src/main.rs
+++ oreilly_system_design/2_quickreplace/src/main.rs
@@ -1,37 +1,9 @@
-use std::env;
-use text_colorizer::*;
+mod parseargs;
+// use std::env;
+
+use crate::parseargs::*;
 
 fn main() {
     let args = parse_args();
     println!("{:?}", args);
-}
-
-#[derive(Debug)]
-struct Arguments {
-    target: String,
-    replacement: String,
-    filename: String,
-    output: String,
-}
-
-fn print_usage() {
-    eprintln!("\n{} - change occurrences of one string into another", "quickreplace".green());
-    eprintln!("Usage: quickreplace <target> <replacement> <input> <output>");
-}
-
-fn parse_args() -> Arguments {
-    let args: Vec<String> = env::args().skip(1).collect();
-
-    if args.len() != 4 {
-        print_usage();
-        eprintln!("{} expected 4 arguments, got {}", "Error:".red().bold(), args.len());
-        std::process::exit(1);
-    }
-
-    Arguments {
-        target: args[0].clone(),
-        replacement: args[1].clone(),
-        filename: args[2].clone(),
-        output: args[3].clone(),
-    }
 }
