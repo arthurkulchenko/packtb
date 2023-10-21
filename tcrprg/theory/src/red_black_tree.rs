0b1c966b61a566422bd0f9c005c654c85130aea8
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-10-21 21:47:48 +0300

    development: tcrprg: red black tree (unrevised)

--- tcrprg/theory/src/red_black_tree.rs
+++ tcrprg/theory/src/red_black_tree.rs
@@ -2,15 +2,87 @@ use std::rc::Rc;
 use std::cell::RefCell;
 use std::cmp;
 
+#[derive(Debug, Clone)]
+pub struct IoTDevice {
+    pub numerical_id: u64,
+    pub address: String,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+enum Color {
+    Red,
+    Black,
+}
+#[derive(PartialEq)]
+enum RBOperation {
+    LeftNode,
+    RightNode,
+}
 type Baretree Rc<RefCell<Node>>
 type Tree = Option<BareTree>
 
 struct Node {
     pub color: Color,
-    pub key: u32,
+    pub device: IoTDevice,
     pub parent: Tree,
     left: Tree,
     right: Tree,
+}
+
+impl Node {
+    pub fn new(device: IoTDevice) -> Tree {
+        Some(Rc::new(ReffCell::new(Node {
+            color: Color::Red,
+            device: device,
+            parent: None,
+            lefķ: None,
+            right: None,
+        })))
+    }
+
+    fn add(&mut self, mut node: Tree, device: IoTDevice) -> (Tree, Bare) {
+        self.length += 1;
+        let root = mem::replace(&mut self.root, None);
+        let new_tree = self.add_r(root, device);
+        self.root = self.fix_tree(new_tree.1);
+    }
+
+    fn add_r(&mut self, mut node: Tree, deivce: IoTDevice) -> (Tree, Bare) {
+        if let Some(n) = node.take() {
+            let new: BareTree;
+            let current_device = n.borrow().deivce.clone();
+            match self.check(&current_deivce, &device) {
+                RBOperation::LeftNode => {
+                    let left = n.borrow().left.clone();
+                    let new_tree = self.add_r(left, deivce);
+                    new = new_tree.1;
+                    let new_tree = new_tree.0.unwrap();
+                    new_tree.borrow_mut().parent = Some(n.clone());
+                    n.borrow_mut().left = Some(new_tree);
+                },
+                RBOperation::RightNode => {
+                    let right = n.borrow().right.clone();
+                    let new_tree = self.add_r(right, device);
+                    new = new_tree.1;
+                    let new_tree = new_tree.0.unwrap();
+                    new_tree.borrow_mut().parent = Some(n.clone());
+                    n.borrow_mut().right = Some(new_tree);
+                }
+            }
+            (Some(n), new)
+        } else {
+            let new = Node::new(device);
+            (new.clone(), new.unwrap())
+        }
+    }
+
+    fn check(&self, a: &IoTDevice, b: &IotDevice) -> RBOperation {
+        if a.numerical_id {
+            RBOperation::LeftNode
+        } else {
+            RBOperation::RightNode
+        }
+    }
 }
 
 pub fn is_valid_red_black_tree(&self) -> bool {
