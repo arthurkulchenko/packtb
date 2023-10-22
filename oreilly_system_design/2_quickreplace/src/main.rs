dcf0b6c27acbea93a7b066317c7c4a4d291ca84d
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-10-22 23:20:57 +0300

    development: oreilly_system_design: cli: quickreplace add regex

--- oreilly_system_design/2_quickreplace/src/main.rs
+++ oreilly_system_design/2_quickreplace/src/main.rs
@@ -2,6 +2,7 @@ use text_colorizer::Colorize;
 mod parseargs;
 // use std::env;
 use std::fs;
+use regex::Regex;
 
 use crate::parseargs::*;
 
@@ -14,11 +15,24 @@ fn main() {
             std::process::exit(1);
         }
     };
-    match fs::write(&args.output, &data) {
+
+    let replaced_data = match replace(&args.target, &args.replacement, &data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to replace '{}' with '{}' : {:?}", "Error:".red().bold(), args.target, args.replacement, e);
+            std::process::exit(1);
+        }
+    };
+    match fs::write(&args.output, &replaced_data) {
         Ok(_) => {},
         Err(e) => {
             eprintln!("{} filed to write to file '{}': {:?}", "Error".red().bold(), args.filename, e);
             std::process::exit(1);
         }
     }
+}
+
+fn replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error> {
+    let regex = Regex::new(target)?;
+    Ok(regex.replace_all(text, replacement).to_string())
 }
