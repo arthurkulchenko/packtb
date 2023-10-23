3fac47c190b003a8ec9b344488d822827c1e4b0a
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-10-24 02:15:02 +0300

    development: microservices: hyper-microservice: add shared state

--- microservices/hyper-microservice/src/main.rs
+++ microservices/hyper-microservice/src/main.rs
@@ -1,7 +1,7 @@
-// use hyper::service::service_fn;
-// use hyper::{Body, Response, Server};
-// use hyper::rt::Future;
+mod templates;
 
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::convert::Infallible;
 use std::net::SocketAddr;
 
@@ -9,27 +9,22 @@ use http_body_util::Full;
 use hyper::body::Bytes;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
-use hyper::{Request, Response};
+use hyper::{Request, Response, Method, StatusCode};
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
 
-// fn main() {
-//     let addr = ([127, 0, 0, 1], 8080).into();
-//     let builder = Server::bind(&addr);
-//     let server = builder.serve(|| {
-//         service_fn_ok(|_| {
-//             Response::new(Body::from("String"))
-//         })
-//     });
-//     let server = server.map(drop);
-//     hyper::rn::run(server);
-// }
+use slab::Slab;
+
+type UserId = u64;
+struct UserData;
+type UserDb = Arc<Mutex<Slab<UserData>>>;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     // We create a TcpListener and bind it to 127.0.0.1:3000
     let listener = TcpListener::bind(addr).await?;
+    let user_db = Arc::new(Mutex::new(Slab::new()));
     // We start a loop to continuously accept incoming connections
     loop {
         let (stream, _) = listener.accept().await?;
@@ -39,14 +34,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         // Spawn a tokio task to serve multiple connections concurrently
         // Finally, we bind the incoming connection to our `handler` service
         // `service_fn` converts our function in a `Service`
+        let user_db = user_db.clone();
         tokio::task::spawn(async move {
-            if let Err(err) = http1::Builder::new().serve_connection(io, service_fn(handler)).await {
-                println!("Error serving connection: {:?}", err);
+            if let Err(err) = http1::Builder::new().serve_connection(
+                io,
+                service_fn(move |request| {
+                    let user_db = user_db.clone();
+                    routes(request, user_db)
+                }),
+            ).await {
+                eprintln!("Error serving connection: {:?}", err);
             }
         });
     }
 }
 
-async fn handler(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+async fn routes(request: Request<hyper::body::Incoming>, user_db: UserDb) -> Result<Response<Full<Bytes>>, Infallible> {
+    match (request.method(), request.uri().path()) {
+        // Routes
+        (&Method::GET, "/hello_page") => Ok(Response::new(Full::new(Bytes::from(templates::HELLO_PAGE)))),
+        (&Method::GET, "/") => Ok(Response::new(Full::new(Bytes::from(templates::ROOT)))),
+        _ => {
+            let response = Response::new("NOT FOUND".into());
+            let (mut parts, body) = response.into_parts();
+            parts.status = StatusCode::NOT_FOUND;
+            Ok(Response::from_parts(parts, body))
+        }
+    }
 }
---
+++ microservices/hyper-microservice/src/templates/mod.rs
@@ -0,0 +1,26 @@
+
+pub const ROOT: &'static str = r#"
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Hyper</title>
+    </head>
+    <body>
+        <h3>Hyper microservice</h3>
+    </body>
+</html>
+"#;
+
+pub const HELLO_PAGE: &'static str = r#"
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Hyper</title>
+    </head>
+    <body>
+        <h3>Hello, World!</h3>
+    </body>
+</html>
+"#;
