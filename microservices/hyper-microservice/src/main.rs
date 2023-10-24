bffa0d2b1c1a0cd5b0f7b2dc0c3c82676bc653d5
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-10-24 23:42:30 +0300

    development: microservices: hyper-microservice: add empty response method

--- microservices/hyper-microservice/src/main.rs
+++ microservices/hyper-microservice/src/main.rs
@@ -11,6 +11,7 @@ use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{Request, Response, Method, StatusCode};
 use hyper_util::rt::TokioIo;
+// use hyper::body::Body;
 use tokio::net::TcpListener;
 
 use slab::Slab;
@@ -59,18 +60,35 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     }
 }
 
+// fn response_with_code(status: StatusCode, body: &str) -> Response<Body<Data = Type, Error = Type>> {
+// fn response_with_code(status: StatusCode, body: &str) -> Response<dyn Body> {
+fn response_with_code(status: StatusCode, body: &str) -> Response<String> {
+    // Response::builder().status(status).body(Body::empty()).unwrap()
+    // ====================================================
+    let response = Response::default();
+    // if body.len() > 0 {
+    //     response.body_mut().push_str(body);
+    // }
+    let (mut parts, body) = response.into_parts();
+    parts.status = status;
+    Response::from_parts(parts, body)
+}
+
 async fn routes(request: Request<hyper::body::Incoming>, user_db: UserDb) -> Result<Response<Full<Bytes>>, Infallible> {
     match (request.method(), request.uri().path()) {
         (&Method::GET, "/hello_page") => Ok(Response::new(Full::new(Bytes::from(templates::HELLO_PAGE)))),
         (&Method::GET, "/") => Ok(Response::new(Full::new(Bytes::from(templates::ROOT)))),
         // (method, path) if path.starts_with(USER_PATH) => {
         (method, path) if USER_PATH.is_match(path) => {
+            // User specific handlers
             let user_id_caps = USER_PATH.captures(path);
             let user_id = user_id_caps.unwrap().name("user_id").unwrap().as_str();
+            // OPTIONAL: Check user_id is a valid u64
+            let users = user_db.lock().unwrap();
             Ok(Response::new(Full::new(Bytes::from(templates::USER_PAGE.replace("user_id", user_id)))))
         },
         _ => {
-            let response = Response::new("NOT FOUND".into());
+            let response = Response::default();
             let (mut parts, body) = response.into_parts();
             parts.status = StatusCode::NOT_FOUND;
             Ok(Response::from_parts(parts, body))
