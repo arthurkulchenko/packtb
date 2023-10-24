486291c93325a7f7c38d2ee99bed6b46ff81c467
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-10-24 23:53:15 +0300

    development: microservices: hyper-microservice: add users POST

--- microservices/hyper-microservice/src/main.rs
+++ microservices/hyper-microservice/src/main.rs
@@ -62,17 +62,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
 // fn response_with_code(status: StatusCode, body: &str) -> Response<Body<Data = Type, Error = Type>> {
 // fn response_with_code(status: StatusCode, body: &str) -> Response<dyn Body> {
-fn response_with_code(status: StatusCode, body: &str) -> Response<String> {
+// fn response_with_code(status: StatusCode, body: &str) -> Response<Full<Bytes>> {
     // Response::builder().status(status).body(Body::empty()).unwrap()
     // ====================================================
-    let response = Response::default();
+    // let response = Response::default();
     // if body.len() > 0 {
     //     response.body_mut().push_str(body);
     // }
-    let (mut parts, body) = response.into_parts();
-    parts.status = status;
-    Response::from_parts(parts, body)
+    // let (mut parts, body) = response.into_parts();
+    // parts.status = status;
+    // Response::from_parts(parts, body);
+    // Response::new(Full::new(Bytes::from(body)))
+// }
+
+fn response_with_code(status: StatusCode) -> Response<Full<Bytes> > {
+    Response::builder().status(status).body(Full::new(Bytes::from(""))).unwrap()
 }
+
+// let response = Response::default();
+//             let (mut parts, body) = response.into_parts();
+//             parts.status = StatusCode::NOT_FOUND;
+//             Ok(Response::from_parts(parts, body))
+// }
 
 async fn routes(request: Request<hyper::body::Incoming>, user_db: UserDb) -> Result<Response<Full<Bytes>>, Infallible> {
     match (request.method(), request.uri().path()) {
@@ -82,12 +93,22 @@ async fn routes(request: Request<hyper::body::Incoming>, user_db: UserDb) -> Res
         (method, path) if USER_PATH.is_match(path) => {
             // User specific handlers
             let user_id_caps = USER_PATH.captures(path);
-            let user_id = user_id_caps.unwrap().name("user_id").unwrap().as_str();
+            // let user_id = user_id_caps.unwrap().name("user_id").unwrap().as_str();
+            let optional_user_id = user_id_caps.unwrap().name("user_id");
             // OPTIONAL: Check user_id is a valid u64
-            let users = user_db.lock().unwrap();
-            Ok(Response::new(Full::new(Bytes::from(templates::USER_PAGE.replace("user_id", user_id)))))
+            let mut users = user_db.lock().unwrap();
+            match (method, optional_user_id) {
+                (&Method::POST, None) => {
+                    let id = users.insert(UserData);
+                    // let response = Response::default();
+                    response_with_code(StatusCode::OK)
+                }
+                _ => response_with_code(StatusCode::METHOD_NOT_ALLOWED),
+            };
+            Ok(Response::new(Full::new(Bytes::from(templates::USER_PAGE.replace("user_id", optional_user_id.unwrap().as_str())))))
         },
         _ => {
+            // Ok(response_with_code(StatusCode::NOT_FOUND, ""));
             let response = Response::default();
             let (mut parts, body) = response.into_parts();
             parts.status = StatusCode::NOT_FOUND;
