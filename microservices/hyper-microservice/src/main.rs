96a66dd70d4e349496c24cfea21e2ec0ba131724
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-10-25 22:04:37 +0300

    development: microservices: hyper-microservice: minor refact

--- microservices/hyper-microservice/src/main.rs
+++ microservices/hyper-microservice/src/main.rs
@@ -12,15 +12,18 @@ use hyper::service::{make_service_fn, service_fn};
 use slab::Slab;
 use regex::Regex;
 use lazy_static::lazy_static;
+use log::info;
 
 lazy_static! {
     static ref INDEX_PATH: Regex = Regex::new("^/(index\\.html?)?$").unwrap();
-    static ref USERS_PATH: Regex = Regex::new("^/users/(\\d+)$").unwrap();
-    // static ref USERS_PATH: Regex = Regex::new("^/users/?$").unwrap()
+    // static ref USERS_PATH: Regex = Regex::new("^/users/(\\d+)$").unwrap();
+    // static ref USERS_PATH: Regex = Regex::new("^/users/?$").unwrap();
+    static ref USERS_PATH: Regex = Regex::new("^/users$").unwrap();
     static ref USER_PATH: Regex = Regex::new("^/users/((?P<user_id>\\d+?)/?)?$").unwrap();
 }
 
 type UserId = u64;
+#[derive(Debug)]
 struct UserData;
 type UserDb = Arc<Mutex<Slab<UserData>>>;
 
@@ -53,24 +56,34 @@ fn response(status: StatusCode, body: &str) -> Response<Body> {
 }
 
 async fn routes(request: Request<Body>, user_db: UserDb) -> Result<Response<Body>, Infallible> {
+    info!("request: {:?}", request);
     match (request.method(), request.uri().path()) {
         (&Method::GET, "/hello_page") => Ok(response(StatusCode::OK, templates::HELLO_PAGE)),
         (&Method::GET, "/") => Ok(response(StatusCode::OK, templates::ROOT)),
         // (method, path) if path.starts_with(USER_PATH) => {
-        (method, path) if USER_PATH.is_match(path) => {
-            let user_id_caps = USER_PATH.captures(path);
-            // let user_id = user_id_caps.unwrap().name("user_id").unwrap().as_str();
-            let optional_user_id = user_id_caps.unwrap().name("user_id");
-            // OPTIONAL: Check user_id is a valid u64
-            let mut users = user_db.lock().unwrap();
+        (method, path) if (USER_PATH.is_match(path) || USERS_PATH.is_match(path)) => {
+            info!("User path");
+            let optional_user_id = USER_PATH.captures(path);
             match (method, optional_user_id) {
                 (&Method::POST, None) => {
-                    let _id = users.insert(UserData) as UserId;
-                    response(StatusCode::OK, "")
+                    let mut users = user_db.lock().unwrap();
+                    let id = users.insert(UserData) as UserId;
+                    drop(users);
+                    Ok(response(StatusCode::OK, &templates::USER_PAGE.replace("user_id", &format!("{}", id))))
+                    // Ok(response(StatusCode::OK, "METHOD_NOT_ALLOWED"))
                 }
-                _ => response(StatusCode::METHOD_NOT_ALLOWED, ""),
-            };
-            Ok(response(StatusCode::OK, &templates::USER_PAGE.replace("user_id", optional_user_id.unwrap().as_str())))
+                (&Method::GET, Some(params_id)) => {
+                    let current_user_id = params_id.name("user_id").unwrap().as_str().parse::<usize>().unwrap();
+                    let users = user_db.lock().unwrap();
+                    if users.contains(current_user_id) {
+                        let current_user = &users[current_user_id];
+                        let display_user = format!("Dedug User: {:?}", current_user);
+                        return Ok(response(StatusCode::OK, &templates::USER_PAGE.replace("user_id", &display_user)))
+                    }
+                    Ok(response(StatusCode::OK, &templates::USER_PAGE.replace("user_id", "No user")))
+                }
+                _ => Ok(response(StatusCode::METHOD_NOT_ALLOWED, "")),
+            }
         },
         _ => Ok(response(StatusCode::NOT_FOUND, ""))
     }
