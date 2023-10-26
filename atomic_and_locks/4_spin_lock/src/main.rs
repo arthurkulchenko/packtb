b20f3edc1560b7a1cc0dc5d0affb3d4df772e1c6
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-10-26 22:57:11 +0300

    development: atomic_and_locks: spin lock: init

---
+++ atomic_and_locks/4_spin_lock/src/main.rs
@@ -0,0 +1,27 @@
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering::{Acquire, Release};
+
+pub struct SpinLock {
+    locked: AtomicBool,
+}
+
+impl SpinLock {
+    pub const fn new() -> Self {
+        Self { locked: AtomicBool::new(false) }
+    }
+
+    pub fn lock(&self) {
+        while self.locked.swap(true, Acquire) {
+            std::hint::spin_loop();
+        }
+    }
+
+    pub fn ulock(&self) {
+        self.locked.store(false, Release);
+    }
+
+}
+
+fn main() {
+    println!("Hello, world!");
+}
